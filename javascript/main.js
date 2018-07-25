0ce6606d6513fa91d65be8ac714b9881eba0a3ca
Neef Rehman <neef.rehman@gmail.com>
2018-07-25 20:46:20 +0100

    photos

---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+var cards = $(".photo");
+for(var i = 0; i < cards.length; i++){
+    var target = Math.floor(Math.random() * cards.length -1) + 1;
+    var target2 = Math.floor(Math.random() * cards.length -1) +1;
+    cards.eq(target).before(cards.eq(target2));
+}
