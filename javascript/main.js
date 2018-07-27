878616678e15f2d063229b1f6f63a2deda850542
Neef Rehman <neef.rehman@gmail.com>
2018-07-27 15:13:44 +0100

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,15 @@
 // Randomise photo order
 
-var cards = $(".photo");
+var cards = $("section");
 for(var i = 0; i < cards.length; i++){
     var target = Math.floor(Math.random() * cards.length -1) + 1;
     var target2 = Math.floor(Math.random() * cards.length -1) +1;
     cards.eq(target).before(cards.eq(target2));
 }
+
+
+// unveil
+
+$(document).ready(function() {
+  $("section").unveil(2000);
+});
