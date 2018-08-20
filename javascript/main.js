538d1d0c1a70913b0c2836938fdf4213b87da1f4
Neef Rehman <neef.rehman@gmail.com>
2018-08-20 17:28:47 +0100

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -5,15 +5,15 @@ $document.mousemove(function(e) {
     var width = $document.width() / 255;
     var pageX = e.pageX / width;
     var pageY = e.pageY / width;
-    var valueX = ( Math.abs(pageX) + 300 ) / 300;
+    var valueX = ( Math.abs(pageX) + 400 ) / 400;
     $("span.stretch").css("transform", "scale(" + valueX + "," + 1 + ")");
 });
 window.ondevicemotion = function(event) {
   var mobileX = event.accelerationIncludingGravity.x;
   var mobileY = event.accelerationIncludingGravity.y;
   var mobileZ = event.accelerationIncludingGravity.z;
-  var valueMobileX = ( Math.abs(mobileX) + 7 ) / 7;
-  var valueMobileY = ( Math.abs(mobileY) + 7 ) / 7;
+  var valueMobileX = ( Math.abs(mobileX) + 8 ) / 8;
+  var valueMobileY = ( Math.abs(mobileY) + 8 ) / 8;
   $("span.stretch").css("transform", "scale(" + valueMobileX + "," + 1 + ")");
   if(window.innerWidth > window.innerHeight) {
     $("span.stretch").css("transform", "scale(" + valueMobileY + "," + 1 + ")");
