a8c8ad80301dc282bd52feff1b27d85188155fbc
Neef Rehman <neef.rehman@gmail.com>
2018-08-20 12:41:47 +0100

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,11 @@
 // Header stretch
-
 var $document = $(document);
 
 $document.mousemove(function(e) {
     var width = $document.width() / 255;
     var pageX = e.pageX / width;
     var pageY = e.pageY / width;
-    var valueX = pageX + 100;
+    var valueX = pageX + 122.5;
     var valueY = pageY;
     $("span.stretch").css("transform", "scale(" + valueX/200 + "," + 1 + ")");
 });
@@ -14,12 +13,11 @@ window.ondevicemotion = function(event) {
   var x = event.accelerationIncludingGravity.x;
   var y = event.accelerationIncludingGravity.y;
   var z = event.accelerationIncludingGravity.z;
-  $("span.stretch").css("transform", "scale(" + (x+3)/5 + "," + 1 + ")");
+  $("span.stretch").css("transform", "scale(" + Math.sqrt( Math.pow(x, 2) )+3/5 + "," + 1 + ")");
 }
 
 
 // Randomise photo order
-
 var cards = $("section");
 for(var i = 0; i < cards.length; i++){
     var target = Math.floor(Math.random() * cards.length -1) + 1;
@@ -29,7 +27,6 @@ for(var i = 0; i < cards.length; i++){
 
 
 // unveil
-
 $(document).ready(function() {
   $("section").unveil(3000);
 });
