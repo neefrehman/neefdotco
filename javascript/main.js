503782dee805dcae4172209e336756081171e8a5
Neef Rehman <neef.rehman@gmail.com>
2018-08-25 17:56:57 +0100

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -5,15 +5,15 @@ $document.mousemove(function(e) {
     var width = $document.width() / 255;
     var pageX = e.pageX / width;
     var pageY = e.pageY / width;
-    var valueX = ( Math.abs(pageX) + 600 ) / 600;
+    var valueX = ( Math.abs(pageX) + 700 ) / 700;
     $("span.stretch").css("transform", "scale(" + valueX + "," + 1 + ")");
 });
 window.ondevicemotion = function(event) {
   var mobileX = event.accelerationIncludingGravity.x;
   var mobileY = event.accelerationIncludingGravity.y;
   var mobileZ = event.accelerationIncludingGravity.z;
-  var valueMobileX = ( Math.abs(mobileX) + 12 ) / 12;
-  var valueMobileY = ( Math.abs(mobileY) + 12 ) / 12;
+  var valueMobileX = ( Math.abs(mobileX) + 14 ) / 14;
+  var valueMobileY = ( Math.abs(mobileY) + 14 ) / 14;
   $("span.stretch").css("transform", "scale(" + valueMobileX + "," + 1 + ")");
   if(window.innerWidth > window.innerHeight) {
     $("span.stretch").css("transform", "scale(" + valueMobileY + "," + 1 + ")");
@@ -28,6 +28,18 @@ for(var i = 0; i < cards.length; i++){
     var target2 = Math.floor(Math.random() * cards.length -1) +1;
     cards.eq(target).before(cards.eq(target2));
 }
+
+
+// Grid view
+$(".grid_toggle").click(function() {
+  $("body").toggleClass("grid");
+  $("section").toggleClass("grid");
+  $("section").trigger("unveil");
+
+  var x = document.getElementById("toggle");
+  if (x.innerHTML === "Grid") { x.innerHTML = "Full"; }
+  else { x.innerHTML = "Grid"; }
+});
 
 
 // unveil
