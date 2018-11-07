95d0baab32076b864f37a483d6d2e5109e5e6bb1
Neef Rehman <neef.rehman@gmail.com>
2018-11-07 11:28:42 +0000

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -22,7 +22,7 @@ window.ondevicemotion = function (event) {
 $(document).ready(function () {
 
     // Create elements & add HTML attributes
-    var n = 53;
+    var n = 55;
     $("footer").after(new Array(++n).join("<div></div>"));
     $.each($("div"), function (index, value) {
         var num = index + 1;
