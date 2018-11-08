250ada948fe4196e27ff030b63633249b757e10c
Neef Rehman <neef.rehman@gmail.com>
2018-11-08 16:32:32 +0000

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,7 @@ $(document).mousemove(function (e) {
     var width = $(document).width() / 255;
     var pageX = e.pageX / width;
     var pageY = e.pageY / width;
-    var valueX = (Math.abs(pageX) + 1500) / 1500;
+    var valueX = (Math.abs(pageX) + 1200) / 1200;
     $("span.stretch").css("transform", "scale(" + valueX + "," + 1 + ")");
 });
 window.ondevicemotion = function (event) {
