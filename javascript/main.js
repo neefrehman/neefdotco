54e3a6bdb41d7857c2d1de244ce453a95d10e6af
Neef Rehman <neef.rehman@gmail.com>
2018-12-07 18:32:26 +0000

    tidy

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,10 @@
 // jshint esversion: 6
+
 // Header stretch
-const stretch = document.querySelector("span.stretch");
 if (document.body.contains(stretch)) {
+
+    const stretch = document.querySelector("span.stretch");
+
     document.onmousemove = function (e) {
         let width = document.body.clientWidth / 255;
         let pageX = e.pageX / width;
@@ -9,6 +12,7 @@ if (document.body.contains(stretch)) {
         let valueX = 0.1 + ((Math.abs(pageX) + 1200) / 1200);
         stretch.style.transform = `scale(${valueX}, 1)`;
     };
+
     window.ondevicemotion = function (e) {
         let mobileX = e.accelerationIncludingGravity.x;
         let mobileY = e.accelerationIncludingGravity.y;
@@ -26,12 +30,13 @@ if (document.body.contains(stretch)) {
 // Photos
 $(document).ready(function () {
 
-    // Create elements & add HTML attributes
+    // Create divs
     let n = 56;
     $("footer").after(new Array(++n).join("<div></div>"));
 
-    const photo = $("div");
-    $.each(photo, function (index, value) {
+    // Declare photos variable & add attributes
+    const photos = $("div");
+    $.each(photos, function (index, value) {
         $(value).attr({
             "data-src": `photos/photo_${++index}.jpg`,
             "class": "grid"
@@ -39,14 +44,14 @@ $(document).ready(function () {
     });
 
     // Randomise order
-    for (let i = 0; i < photo.length; i++) {
-        let target = Math.floor(Math.random() * photo.length -1) + 1;
-        let target2 = Math.floor(Math.random() * photo.length -1) + 1;
-        photo.eq(target).before(photo.eq(target2));
+    for (let i = 0; i < photos.length; i++) {
+        let target = Math.floor(Math.random() * photos.length -1) + 1;
+        let target2 = Math.floor(Math.random() * photos.length -1) + 1;
+        photos.eq(target).before(photos.eq(target2));
     }
 
     // unveil
-    photo.unveil(2000);
+    photos.unveil(2000);
 
     // Grid view
     $("#grid_toggle").click(function () {
@@ -64,7 +69,7 @@ $(document).ready(function () {
         $(document).scrollTop(decimalScrolled * newPageHeight);
 
         // Re-unveil
-        photo.unveil(2000);
+        photos.unveil(2000);
 
         // Toggle button text & update URL
         if (this.innerHTML === "Full screen") {
@@ -83,15 +88,15 @@ $(document).ready(function () {
     }
 
     // Zoom photos on click
-    photo.click(function () {
+    photos.click(function () {
         $(this).toggleClass("zoom")
         .siblings().removeClass("zoom");
     });
 
     // Remove zoom on whitespace click
     $(document).click(function (e) {
-        if (!photo.is(e.target) && photo.has(e.target).length === 0) {
-            photo.removeClass("zoom");
+        if (!photos.is(e.target) && photos.has(e.target).length === 0) {
+            photos.removeClass("zoom");
         }
     });
 
