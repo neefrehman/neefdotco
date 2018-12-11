73d965d86ad2509f0d6412f7829103bf0ffdad31
Neef Rehman <neef.rehman@gmail.com>
2018-12-11 16:47:35 +0000

    remove all stretch ping pong

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,25 @@
 // Header stretch
-const title = document.querySelector("span.stretch");
-if (document.body.contains(title)) {
+const stretch = document.querySelector("span.stretch");
+if (document.body.contains(stretch)) {
 
     document.onmousemove = function(e) {
         let width = document.body.clientWidth / 255;
-        let mousePosition = e.pageX / width;
-        let stretchValue = 0.1 + (mousePosition + 1200) / 1200;
-        title.style.transform = `scale(${stretchValue}, 1)`;
+        let pageX = e.pageX / width;
+        let pageY = e.pageY / width;
+        let valueX = 0.1 + ((Math.abs(pageX) + 1200) / 1200);
+        stretch.style.transform = `scale(${valueX}, 1)`;
     };
 
     window.ondevicemotion = function(e) {
-        let deviceTilt = e.accelerationIncludingGravity.x;
-        if (window.innerHeight < window.innerWidth) deviceTilt = e.accelerationIncludingGravity.y;
-        let stretchValue = 0.1 + ((Math.abs(deviceTilt) + 16) / 16);
-        title.style.transform = `scale(${stretchValue}, 1)`;
+        let mobileX = e.accelerationIncludingGravity.x;
+        let mobileY = e.accelerationIncludingGravity.y;
+        let valueX = 0.1 + ((Math.abs(mobileX) + 16) / 16);
+        let valueY = 0.1 + ((Math.abs(mobileY) + 16) / 16);
+        if (window.innerHeight > window.innerWidth) {
+            stretch.style.transform = `scale(${valueX}, 1)`;
+        } else {
+            stretch.style.transform = `scale(${valueY}, 1)`;
+        }
     };
 
 }
