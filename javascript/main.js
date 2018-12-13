46277e36f3b0792d838491d35dafaf60facb7dd4
Neef Rehman <neef.rehman@gmail.com>
2018-12-13 11:59:07 +0000

    window & document height back to jquery

--- javascript/main.js
+++ javascript/main.js
@@ -50,7 +50,7 @@ if (document.body.contains(photoContainer)) {
 
         // Scroll variables (pre-change)
         let scrollValue = document.documentElement.scrollTop;
-        let pageHeight = document.documentElement.scrollHeight - window.visualViewport.height;
+        let pageHeight = $(document).height() - $(window).height();
         let decimalScrolled = scrollValue / pageHeight;
 
         // Execute grid view
@@ -58,7 +58,7 @@ if (document.body.contains(photoContainer)) {
         photos.forEach(photo => photo.classList.toggle("grid"));
 
         // Maintain relative scroll height
-        let newPageHeight = document.documentElement.scrollHeight - window.visualViewport.height;
+        let newPageHeight = $(document).height() - $(window).height();
         document.documentElement.scrollTop = decimalScrolled * newPageHeight;
 
         // Toggle button text & update URL
