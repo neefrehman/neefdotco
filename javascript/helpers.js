ec62c20c714c8d330cee8f41ff973c0d0c36d8e0
Neef Rehman <neef.rehman@gmail.com>
2019-01-30 19:37:19 +0000

    quadspace the ting

--- javascript/helpers.js
+++ javascript/helpers.js
@@ -1,34 +1,34 @@
 const lzy = (offset = 200) => {
 
-  const images = document.querySelectorAll("[data-src]");
-  const observer = new IntersectionObserver(onIntersection, {
-    rootMargin: `${offset}px ${offset}px`,
-    threshold: 0.01
-  });
-
-  function loadImage(imageEl) {
-    imageEl.style.backgroundImage = `url(${imageEl.getAttribute("data-src")})`;
-    imageEl.removeAttribute("data-src");
-  }
-
-  images.forEach(image => observer.observe(image));
-
-  function onIntersection(entries) {
-    entries.forEach(entry => {
-      if (entry.intersectionRatio > 0) {
-        observer.unobserve(entry.target);
-        loadImage(entry.target);
-      }
+    const images = document.querySelectorAll("[data-src]");
+    const observer = new IntersectionObserver(onIntersection, {
+        rootMargin: `${offset}px ${offset}px`,
+        threshold: 0.01
     });
-  }
+
+    function loadImage(imageEl) {
+        imageEl.style.backgroundImage = `url(${imageEl.getAttribute("data-src")})`;
+        imageEl.removeAttribute("data-src");
+    }
+
+    images.forEach(image => observer.observe(image));
+
+    function onIntersection(entries) {
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+                observer.unobserve(entry.target);
+                loadImage(entry.target);
+            }
+        });
+    }
 
 };
 
 
 const shuffle = a => {
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
 };
