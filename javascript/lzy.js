d903fd4815b4646d95ec9974b94af742677c0120
Neef Rehman <neef.rehman@gmail.com>
2019-02-18 10:58:20 +0000

    name changes & JS tidy

--- javascript/lzy.js
+++ javascript/lzy.js
@@ -1,6 +1,16 @@
 const lzy = (offset = 200) => {
 
     const images = document.querySelectorAll("[data-src]");
+    
+    const onIntersection = entries => {
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+                observer.unobserve(entry.target);
+                loadImage(entry.target);
+            }
+        });
+    };
+
     const observer = new IntersectionObserver(onIntersection, {
         rootMargin: `${offset}px ${offset}px`,
         threshold: 0.01
@@ -13,22 +23,4 @@ const lzy = (offset = 200) => {
 
     images.forEach(image => observer.observe(image));
 
-    function onIntersection(entries) {
-        entries.forEach(entry => {
-            if (entry.intersectionRatio > 0) {
-                observer.unobserve(entry.target);
-                loadImage(entry.target);
-            }
-        });
-    }
-
-};
-
-
-const shuffle = a => {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
 };
--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,13 @@
+// Helpers
+const shuffle = a => {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+};
+
+
 // Dark mode (localStorage trigger in html <head>)
 const darkModeToggle = document.documentElement;
 const darkMode = () => document.documentElement.classList.toggle("dark");
