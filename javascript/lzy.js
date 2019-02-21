9d942c6a34a5cc6c4f6f1e148b4bb8f7d256eeef
Neef Rehman <neef.rehman@gmail.com>
2019-02-21 15:58:22 +0000

    instagram browser fix

--- javascript/lzy.js
+++ javascript/lzy.js
@@ -1,26 +1,33 @@
+var ua = navigator.userAgent || navigator.vendor || window.opera;
+var isInstagram = (ua.indexOf('Instagram') > -1) ? true : false;
+
 const lzy = (offset = 200) => {
 
     const images = document.querySelectorAll("[data-src]");
-    
-    const onIntersection = entries => {
-        entries.forEach(entry => {
-            if (entry.intersectionRatio > 0) {
-                observer.unobserve(entry.target);
-                loadImage(entry.target);
-            }
+
+    if (isInstagram) {
+        images.forEach(image => image.setAttribute("src", image.getAttribute("data-src")));
+    } else {
+        const onIntersection = entries => {
+            entries.forEach(entry => {
+                if (entry.intersectionRatio > 0) {
+                    observer.unobserve(entry.target);
+                    loadImage(entry.target);
+                }
+            });
+        };
+
+        const observer = new IntersectionObserver(onIntersection, {
+            rootMargin: `${offset}px ${offset}px`,
+            threshold: 0.01
         });
-    };
 
-    const observer = new IntersectionObserver(onIntersection, {
-        rootMargin: `${offset}px ${offset}px`,
-        threshold: 0.01
-    });
+        const loadImage = imageEl => {
+            imageEl.style.backgroundImage = `url(${imageEl.getAttribute("data-src")})`;
+            imageEl.removeAttribute("data-src");
+        };
 
-    const loadImage = imageEl => {
-        imageEl.style.backgroundImage = `url(${imageEl.getAttribute("data-src")})`;
-        imageEl.removeAttribute("data-src");
-    };
-
-    images.forEach(image => observer.observe(image));
+        images.forEach(image => observer.observe(image));
+    }
 
 };
