86b9bdb2a66c4cc31ae1f688cacea215eed48f6f
Neef Rehman <neef.rehman@gmail.com>
2019-03-23 19:06:08 +0000

    add pic and small pic size 1000 -> 800

--- javascript/main.js
+++ javascript/main.js
@@ -48,7 +48,7 @@ const photoContainer = document.querySelector(".photo-container");
 if (photoContainer) {
 
     // Photo array variables
-    const n = 69;
+    const n = 70;
     const size = (window.innerWidth > 500) ? "large" : "small";
     const r = () => 10 * (Math.floor(Math.random() * 9)) - 40;
 
