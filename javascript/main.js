7ba9d5cb99d102a48663605765f57129800f31e0
Neef Rehman <neef.rehman@gmail.com>
2019-05-06 22:42:12 +0100

    media query trigger ferom main.js -> head of html files

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,9 @@
-// Dark mode (localStorage trigger in html <head>)
+// Dark mode (localStorage & media query trigger in html <head>)
 const darkModeButton = document.querySelector(".dark-mode-button");
-const darkModeLocal = localStorage.getItem("darkMode");
-const hasDarkUserPref = window.matchMedia("(prefers-color-scheme: dark)").matches;
-let isDark = document.documentElement.classList.contains("dark");
 
 const darkMode = () => {
     document.documentElement.classList.toggle("dark");
-    isDark = document.documentElement.classList.contains("dark");
+    const isDark = document.documentElement.classList.contains("dark");
     isDark ? localStorage.setItem("darkMode", 1) : localStorage.setItem("darkMode", 0);
 };
 
@@ -23,8 +20,6 @@ if (darkModeButton) {
         });
     });
 }
-
-if (hasDarkUserPref && !darkModeLocal && !isDark) darkMode();
 
 
 // Header stretch
