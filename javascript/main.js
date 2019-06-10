bcd4d07b175ede823864250595e5a08b921d5ca3
Neef Rehman <neef.rehman@gmail.com>
2019-06-10 20:38:58 +0100

    site restructure

--- javascript/main.js
+++ javascript/main.js
@@ -34,7 +34,7 @@ if (title && window.DeviceMotionEvent) {
         const deviceTilt = (window.innerHeight > window.innerWidth)
             ? e.accelerationIncludingGravity.x
             : e.accelerationIncludingGravity.y;
-        const stretchValue = 1.1 + (Math.abs(deviceTilt) / 15);
+        const stretchValue = 1.05 + (Math.abs(deviceTilt) / 20);
         title.style.transform = `scale(${stretchValue}, 1)`;
     });
 }
