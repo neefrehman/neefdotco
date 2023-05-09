d937d26aff95930b8b31443804a08d814d9992fe
Neef Rehman <neef.rehman@gmail.com>
2023-05-09 19:06:17 +0100

    misc improvements

--- scripts/process-photos.js
+++ scripts/process-photos.js
@@ -13,9 +13,36 @@ const cacheJsonFile = "./src/data/quantized-photo-colors.json";
 const rawPhotos = fs.readdirSync(rawPhotosDirectory).filter(name => name !== ".DS_Store");
 const processedPhotos = JSON.parse(fs.readFileSync(cacheJsonFile));
 
+const photosToDelete = Object.keys(processedPhotos).filter(file => !rawPhotos.includes(file));
 const photosToProcess = rawPhotos.filter(
   photoName => !Object.keys(processedPhotos).includes(photoName)
 );
+
+const updateCache = () =>
+  fs.writeFileSync(cacheJsonFile, JSON.stringify(processedPhotos, null, 4));
+
+const deleteRemovedPhotos = () => {
+  console.log(`photos that have been removed: ${photosToDelete}`, "\n");
+
+  photosToDelete.forEach(photo => {
+    if (fs.existsSync(`./src/photos/large/${photo}`)) {
+      fs.unlinkSync(`./src/photos/large/${photo}`);
+      console.log(`deleted ${photo}`, "\n");
+    }
+    delete processedPhotos[photo];
+    updateCache();
+  });
+};
+
+const updateFileExtension = file => {
+  let newFile = file;
+  if (file.includes(".jpeg" || file.includes(".JPG"))) {
+    console.log(`renaming extension for ${file}`);
+    newFile = file.replace("jpeg", "jpg").replace("JPG", "jpg");
+    fs.renameSync(`${rawPhotosDirectory}/${file}`, `${rawPhotosDirectory}/${newFile}`);
+  }
+  return newFile;
+};
 
 const extractColorFromImage = async file => {
   const rgb2hex = (r, g, b) =>
@@ -42,14 +69,11 @@ const extractColorFromImage = async file => {
   return colorHex;
 };
 
-const updateFileExtension = file => {
-  let newFile = file;
-  if (file.includes("jpeg")) {
-    newFile = file.replace("jpeg", "jpg");
-    console.log(`renaming extension for ${file}`);
-    fs.renameSync(`${rawPhotosDirectory}/${file}`, `${rawPhotosDirectory}/${newFile}`);
-  }
-  return newFile;
+const resizeImage = async file => {
+  await sharp(`${rawPhotosDirectory}/${file}`)
+    .resize({ width: 1920 })
+    .withMetadata()
+    .toFile(`${rawPhotosDirectory}/../large/${file}`);
 };
 
 const processPhotos = async () => {
@@ -57,17 +81,18 @@ const processPhotos = async () => {
     const newFile = updateFileExtension(file);
     const extractedColor = await extractColorFromImage(newFile);
     processedPhotos[file] = extractedColor;
-    fs.writeFileSync(cacheJsonFile, JSON.stringify(processedPhotos, null, 4));
+    updateCache();
 
     // Also resize the photos so that they aren't too big to check into the repo, or for Astro to process
-    await sharp(`${rawPhotosDirectory}/${file}`)
-      .resize({ width: 1920 })
-      .withMetadata()
-      .toFile(`${rawPhotosDirectory}/../large/${file}`);
+    await resizeImage(`${rawPhotosDirectory}/${file}`);
   }
 };
 
 const main = () => {
+  if (photosToDelete.length > 0) {
+    deleteRemovedPhotos();
+  }
+
   if (photosToProcess.length === 0) {
     console.log("🙌🏾 no new photos to process 🙌🏾");
     return;
