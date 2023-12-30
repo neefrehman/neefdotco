f9c8bdb228f016697368d71f358d6a01c34beb7d
Neef Rehman <neef.rehman@gmail.com>
2023-12-30 16:50:04 +0000

    bump deps and update astro picture generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config";
-import image from "@astrojs/image";
 
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     tailwind({
       applyBaseStyles: false,
     }),
