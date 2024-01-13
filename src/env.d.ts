a28cd3281103ea3c2ee2504b136663e32c3a1c60
Neef Rehman <neef.rehman@gmail.com>
2024-01-13 15:43:59 +0000

    preload pages during transitions [skip ci]

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="astro/client" />
+
+declare interface HTMLImageElement extends HTMLElement {
+  fetchPriority?: "high" | "low" | "auto";
+}
+declare interface HTMLLinkElement extends HTMLElement {
+  fetchPriority?: "high" | "low" | "auto";
+}
