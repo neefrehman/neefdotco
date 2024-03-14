df8018e34f7d594c4130ea3c87ce445739bcdb38
Neef Rehman <neef.rehman@gmail.com>
2024-03-14 19:59:19 +0000

    optimise interactions, add chat!

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,7 +1,4 @@
-export const throttle = <F extends (...args: Parameters<F>) => ReturnType<F>>(
-  fn: F,
-  delay: number
-) => {
+export const throttle = <F extends (...args: Parameters<F>) => ReturnType<F>>(fn: F, delay: number) => {
   let inThrottle: boolean;
   return function (...args: Parameters<F>) {
     if (inThrottle) return;
@@ -9,23 +6,4 @@ export const throttle = <F extends (...args: Parameters<F>) => ReturnType<F>>(
     setTimeout(() => (inThrottle = false), delay);
     fn.apply(this, args);
   };
-};
-
-export const throttleWithLag = <F extends (...args: Parameters<F>) => ReturnType<F>>(
-  fn: F,
-  delay: number,
-  /** How long after the final invocation should the function continue to be called for (in ms) */
-  finalDelay: number
-) => {
-  let updateInterval: NodeJS.Timeout;
-  let endTimeout: NodeJS.Timeout;
-  // @ts-expect-error -- TS can't determine that this type won't be instantiated differently
-  const fnWithInterval: typeof fn = (...args) => {
-    clearTimeout(updateInterval);
-    clearTimeout(endTimeout);
-    updateInterval = setInterval(() => fn(...args), delay);
-    endTimeout = setTimeout(() => clearTimeout(updateInterval), finalDelay);
-    return fn.apply(this, args);
-  };
-  return throttle(fnWithInterval, delay);
 };
