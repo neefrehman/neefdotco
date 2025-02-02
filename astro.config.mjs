b27652c0ff0d381c1b67ea9d38496210d0d5151c
Neef Rehman <neef.rehman@gmail.com>
2025-02-02 17:45:36 +0000

    bump deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-      nesting: true,
-    }),
-  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- src/env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="astro/client" />
-
-declare interface HTMLImageElement extends HTMLElement {
-  fetchPriority?: "high" | "low" | "auto";
-}
-declare interface HTMLLinkElement extends HTMLElement {
-  fetchPriority?: "high" | "low" | "auto";
-}
