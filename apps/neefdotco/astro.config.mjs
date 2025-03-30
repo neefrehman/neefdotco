724fbad29d0817616b82b5a09a4dc7c8836823c8
Neef Rehman <neef.rehman@gmail.com>
2025-03-30 15:07:06 +0100

    Add Netlify adapter

--- apps/neefdotco/astro.config.mjs
+++ apps/neefdotco/astro.config.mjs
@@ -1,8 +1,11 @@
+import netlify from "@astrojs/netlify";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
+  output: "static",
+  site: "https://neef.co",
+  adapter: netlify({ imageCDN: false }),
   vite: {
     plugins: [tailwindcss()],
   },
